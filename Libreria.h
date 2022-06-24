c2686729d76cd501042f91f0b45c17320f68b917
linoabraham <89022192+linoabraham@users.noreply.github.com>
2022-06-24 11:32:03 -0500

    Add files via upload

---
+++ Libreria.h
@@ -0,0 +1,21 @@
+#pragma once
+#pragma once
+#include <string>
+#include <vector>
+#include <fstream>
+#include <list>
+#include <sstream>
+#include <iostream>
+#include <functional>
+#include <iterator>
+#include <stack>
+#include <queue>
+#include <algorithm>
+#include <windows.h>
+#include <conio.h>
+#include <time.h>
+#include <stdlib.h>
+#include <math.h>
+
+
+using namespace std;
