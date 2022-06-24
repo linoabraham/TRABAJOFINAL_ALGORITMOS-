5c3752bc67e4ed95425d34feb9f55a3850974bab
linoabraham <89022192+linoabraham@users.noreply.github.com>
2022-06-24 11:31:48 -0500

    Add files via upload

---
+++ Fecha.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <iostream>
+#include "ListaDoblementeEnlazada.h"
+#include <string>
+class Fecha {
+public:
+    int dia, mes, anio;
+    Fecha() {
+        dia = 1;
+        mes = 1;
+        anio = 1;
+    }
+};
+
